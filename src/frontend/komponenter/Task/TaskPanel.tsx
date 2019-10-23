3f52e7f4a998bec9d3f03d67af05d00a2a1350da
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-23 21:15:29 +0200

    Legger til enkel visning av loggmeldinger.

--- src/frontend/komponenter/Task/TaskPanel.tsx
+++ src/frontend/komponenter/Task/TaskPanel.tsx
@@ -4,14 +4,17 @@ import { Knapp } from 'nav-frontend-knapper';
 import PanelBase from 'nav-frontend-paneler';
 import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import * as React from 'react';
-import { ITaskDTO, taskStatusTekster, taskTypeTekster } from '../../typer/task';
+import { ITaskDTO, ITaskLogg, taskStatusTekster, taskTypeTekster } from '../../typer/task';
 import { actions, useTaskDispatch } from '../TaskProvider';
+import { formatException } from './format';
 
 interface IProps {
     taskDTO: ITaskDTO;
 }
 
 const TaskPanel: React.StatelessComponent<IProps> = ({ taskDTO }) => {
+    const [visLogg, settVisLogg] = React.useState(false);
+
     const tasksDispatcher = useTaskDispatch();
     const task = taskDTO.task;
 
@@ -45,6 +48,31 @@ const TaskPanel: React.StatelessComponent<IProps> = ({ taskDTO }) => {
                 <Normaltekst
                     children={moment(task.opprettetTidspunkt).format('DD.MM.YYYY HH:mm')}
                 />
+            </div>
+
+            <Knapp
+                className={'taskpanel__vislogg'}
+                mini={true}
+                onClick={() => settVisLogg(!visLogg)}
+            >
+                {`${visLogg ? 'Skjul' : 'Vis'} logg`}
+            </Knapp>
+
+            <div className={classNames('taskpanel__logg', visLogg ? '' : 'skjul')}>
+                {task.logg.map((logg: ITaskLogg, index: number) => {
+                    const feilmelding = logg.feilmelding ? JSON.parse(logg.feilmelding) : undefined;
+                    return (
+                        <div key={index} className={'taskpanel__logg--item'}>
+                            <Element children={logg.type} />
+                            {feilmelding && (
+                                <pre
+                                    className={'taskpanel__logg--item-stacktrace'}
+                                    children={feilmelding.stacktrace}
+                                />
+                            )}
+                        </div>
+                    );
+                })}
             </div>
         </PanelBase>
     );
